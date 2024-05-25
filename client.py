45becaf6482d49e811eb127f1b97414d623dafcc
202100628 <202100628@stu.uob.edu.bh>
2024-05-25 06:07:01 +0300

    Update client.py

--- client.py
+++ client.py
@@ -5,11 +5,27 @@ SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 65432
 
 def transmitRequest(socket_client, request_message):
-
-  pass
+  socket_client.send(request_message.encode('utf-8'))
+  length_response_str = socket_client.recv(10).decode('utf-8').strip()
+  print(f"Received response length string: '{length_response_str}'")
+  length_response = int(length_response_str)
+  print(f"Received response length: {length_response}")
+  data_response = b""
+  while len(data_response) < length_response:
+    part_data = socket_client.recv(length_response - len(data_response))
+    data_response += part_data
+  return json.loads(data_response.decode('utf-8'))
 
 def main():
+  username_client = input("Enter your name: ")
+  socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  socket_client.connect((SERVER_HOST, SERVER_PORT))
+  socket_client.send(username_client.encode('utf-8'))
 
+  while True:
+    pass
+
+  socket_client.close()
 
 if __name__ == '__main__':
   main()
