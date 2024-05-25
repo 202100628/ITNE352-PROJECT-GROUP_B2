1c8447e6e7642d3352a1de0c11df4b37255355ea
202100628 <202100628@stu.uob.edu.bh>
2024-05-25 06:00:41 +0300

    Create Server.py

---
+++ Server.py
@@ -0,0 +1,12 @@
+import socket
+import json
+
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 65432
+
+def transmitRequest(socket_client, request_message):
+
+def main():
+
+if __name__ == '__main__':
+  main()
