32aa72c94c47933b7a3772842774a4b4ba85738c
202100628 <202100628@stu.uob.edu.bh>
2024-05-25 06:03:36 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,8 +1,19 @@
 import socket
 import threading
+import requests  
 
 ADDRESS_HOST = '127.0.0.1'
 NUMBER_PORT = 65432
+
+def processClientRequest(socket_client, address):
+  print(f"Accepted connection from {address}")
+  try:
+
+  except ConnectionResetError:
+
+  finally:
+    print(f"Client disconnected")
+    socket_client.close()
 
 def initializeServer():
   server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,6 +22,9 @@ def initializeServer():
   print(f'Server listening on {ADDRESS_HOST}:{NUMBER_PORT}')
 
   while True:
+    socket_client, address = server_socket.accept()
+    client_thread = threading.Thread(target=processClientRequest, args=(socket_client, address))
+    client_thread.start()
 
 if __name__ == '__main__':
   initializeServer()
