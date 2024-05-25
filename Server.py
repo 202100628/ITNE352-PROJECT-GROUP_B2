ce4bed423477d5631418c3ba4a9484b3fc8fdff4
202100628 <202100628@stu.uob.edu.bh>
2024-05-25 06:05:56 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,6 +1,6 @@
 import socket
 import threading
-import requests  
+import requests
 
 ADDRESS_HOST = '127.0.0.1'
 NUMBER_PORT = 65432
@@ -8,9 +8,22 @@ NUMBER_PORT = 65432
 def processClientRequest(socket_client, address):
   print(f"Accepted connection from {address}")
   try:
-
+    username_client = socket_client.recv(1024).decode('utf-8')
+    print(f"Client name: {username_client}")
+    while True:
+      client_request = socket_client.recv(1024).decode('utf-8')
+      if not client_request:
+          break
+      print(f"Requester: {username_client}, Request: {client_request}")
+      if client_request.startswith('get_news'):
+          response_message = "Under development! News functionality coming soon."
+          response_message = response_message.encode('utf-8')
+          socket_client.sendall(response_message)
+      else:
+          error_message = json.dumps({'status': 'error', 'message': 'Invalid request'}).encode('utf-8')
+          socket_client.sendall(error_message)
   except ConnectionResetError:
-
+    pass
   finally:
     print(f"Client disconnected")
     socket_client.close()
